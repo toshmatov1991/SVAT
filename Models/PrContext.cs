2a4131fefe70f2e370cede39720d5fea5e7c4b7a
Тимур Тошматов <kamentimur1991@gmail.com>
2023-03-22 13:04:04 +0700

    авто

--- Models/PrContext.cs
+++ Models/PrContext.cs
@@ -22,7 +22,6 @@ public partial class PrContext : DbContext
     public virtual DbSet<Work> Works { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlite("Filename=pr.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
