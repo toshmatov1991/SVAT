60b9f30f0e8a5017544bb7e7334ef7d5ba2cbeae
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2023-03-28 09:24:32 +0700

    арпапр

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,24 @@ namespace Soliders
 
 
 
+        //Метод хэширования вводимого пароля
+        private string GH(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return Convert.ToBase64String(hash);
+        }
 
+        private void openWin_Click(object sender, RoutedEventArgs e)
+        {
+            //При нажатии на кнопку Войти
+            if(string.IsNullOrEmpty(polzovatel.Text) && string.IsNullOrEmpty(password_user.Password)
+                || string.IsNullOrEmpty(polzovatel.Text) || string.IsNullOrEmpty(password_user.Password))
+            {
+                MessageBox.Show("Вы пропустили одно или несколько полей", "Внимательно!", MessageBoxButton.OK, MessageBoxImage.None);
+            }
+            else
+                MessageBox.Show("Все хорошо", "Молодец!", MessageBoxButton.OK, MessageBoxImage.None);
+        }
     }
 }
