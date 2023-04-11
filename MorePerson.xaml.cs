5c3bf8d96b55e06856a1dbabb0eafac0b56210d3
toshmatov <toshmatov@IT-OKTYABRSK>
2023-04-11 17:08:52 +0700

    123

--- MorePerson.xaml.cs
+++ MorePerson.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Soliders.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,9 +18,52 @@ namespace Soliders
    
     public partial class MorePerson : Window
     {
+        int mestniyId = 0;
         public MorePerson(int idMore)
         {
             InitializeComponent();
+            mestniyId = idMore;
+            PullUpPersona();
+        }
+
+             /* family.Text, 
+                name.Text, 
+                lastname.Text, 
+                datapic.Text, 
+                adressPropiska.Text, 
+                adressFact.Text, 
+                familyStatus.Text, 
+                category.Text, 
+                children.Text, 
+                socialStatus.Text,
+                snils.Text,
+                statusProsto.Text,
+                serial.Text,
+                number.Text
+             */
+
+        private void PullUpPersona()
+        {
+            using (PrContext db = new())
+            {
+                //Запрос
+                var getMyPerson = db.Conscripts.Where(u => u.Id == mestniyId).ToList();
+
+                family.Text = getMyPerson.First().Firstname;
+                name.Text = getMyPerson.First().Name;
+                lastname.Text = getMyPerson.First().Lastname;
+                datapic.Text = getMyPerson.First().Dateof;
+                adressPropiska.Text = getMyPerson.First().Address;
+                adressFact.Text = getMyPerson.First().AddressNext;
+                familyStatus.Text = getMyPerson.First().FamilyStatus;
+                category.Text = getMyPerson.First().Category;
+                children.Text = getMyPerson.First().Children.ToString();
+                socialStatus.Text = getMyPerson.First().SocialStatus;
+                snils.Text = getMyPerson.First().Snils;
+                statusProsto.Text = getMyPerson.First().Status;
+                serial.Text = getMyPerson.First().Passport.Substring(0, 4);
+                number.Text = getMyPerson.First().Passport.Substring(4);
+            }
         }
 
 
